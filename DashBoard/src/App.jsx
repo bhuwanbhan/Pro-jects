a2ac30c5191885fa909c2e4b7f7965dde3e316c0
Bhuban Bhandari <bhandaribhuwan880@gmail.com>
2025-02-06 08:19:21 +0545

    working

--- DashBoard/src/App.jsx
+++ DashBoard/src/App.jsx
@@ -6,11 +6,11 @@ import Main from './Components/MainDash/Main'
 import Header from './Components/Header/Header'
 import Sidebar from './Components/Siderbar/Sidebar';
 // import Layout from './Components/Layouts/Layout';
-// import SignIn1 from './Components/Authentication/Simple/SignIn1';
-// import SignUp2 from './Components/Authentication/Simple/SignUp2';
-// import Forget from './Components/Authentication/Simple/Forget';
-// import ResetPassword from './Components/Authentication/Simple/ResetPassword';
-// import Footer from './Components/Footer/Footer';
+import SignIn1 from './Components/Authentication/Simple/SignIn1';
+import SignUp2 from './Components/Authentication/Simple/SignUp2';
+import Forget from './Components/Authentication/Simple/Forget';
+import ResetPassword from './Components/Authentication/Simple/ResetPassword';
+import Footer from './Components/Footer/Footer';
 
 
  
@@ -24,12 +24,12 @@ function App() {
          
       
            <Main />
-          {/* <SignIn1 />
+          <SignIn1 />
         <SignUp2 />
          <Forget />
          <ResetPassword />
          <Footer />
-          */}
+         
     </>  
      
     
