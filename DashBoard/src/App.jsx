ee382de5fced08fd228e0019868725490ea17430
Pemba Gelu Sherpa <pembagg122@gmail.com>
2025-02-11 15:41:53 +0545

    Yooo

--- DashBoard/src/App.jsx
+++ DashBoard/src/App.jsx
@@ -2,14 +2,12 @@
 // import "bootstrap/dist/js/bootstrap.bundle.min";
 
 import Main from './Components/MainDash/Main'
-import Header from './Components/Header/Header'
 import Sidebar from './Components/Siderbar/Sidebar';
  import Layout from './Components/Layouts/Layout';
 import SignIn1 from './Components/Authentication/SignIn1';
 import SignUp2 from './Components/Authentication/SignUp2';
 import Forget from './Components/Authentication/Forget';
 import ResetPassword from './Components/Authentication/ResetPassword';
-import Footer from './Components/Footer/Footer';
 import {BrowserRouter as Router, Routes, Route,Link } from 'react-router-dom';
 
 
